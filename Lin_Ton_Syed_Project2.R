3054099fd26138cf522335e4e4696e67ac79e877
tonynlin123 <anl5ef@virginia.edu>
2016-12-03 15:01:07 -0500

    Updated comments, plot for last part of 1

--- Lin_Ton_Syed_Project2.R
+++ Lin_Ton_Syed_Project2.R
@@ -15,8 +15,10 @@ blood$donated.2007 <- as.factor(blood$donated.2007)
 #Check attribute types
 sapply(blood, class)
 
-#Normalize the data because of the wide range of numbers found in the dataset
+#Normalize each column of the data because of the wide range of numbers found in the dataset, particularly with the volume in cc's
 blood2 <- cbind(data.Normalization(blood[-5], "n4", "column"), blood[,5])
+
+#Assign appropriate column names
 colnames(blood2) <- c("recency.mo", "frequency", "blood.cc", "time.mo", "donated.2007")
 
 
@@ -28,37 +30,38 @@ colnames(blood2) <- c("recency.mo", "frequency", "blood.cc", "time.mo", "donated
 #target doners and those who may deliver the best future value is important for a blood bank.
 
 #Classification with a neural net. Modified the sample code provided
-set.seed(11)
+#Set a seed, determine the sample size, and establish the sample command
+set.seed(1)
 size.sample <- 571 #Sample size is currently 75% of the data
-
-## Use "sample" command to randomize data and choose a subset to be the training data
 training.sample <- sample(1:nrow(blood2), size.sample)
-blood.train <- blood2[training.sample,] # get a training sample from iris
+
+# With training.sample, split the blood data into training and test sets
+blood.train <- blood2[training.sample,]
 blood.test <- blood2[-training.sample,]
-## Give the data a more transparent name
+
+# Give the data a more transparent name
 nnet_blood.train <- blood.train
 
 # Binarize the categorical output
-nnet_blood.train <- cbind(nnet_blood.train, blood.train$donated.2007 == "0")
-nnet_blood.train <- cbind(nnet_blood.train, blood.train$donated.2007 == "1")
+nnet_blood.train <- cbind(nnet_blood.train, blood.train$donated.2007 == 0)
+nnet_blood.train <- cbind(nnet_blood.train, blood.train$donated.2007 == 1)
 
-## Notice,here, we are adding columns to iristrain, each with the name of a possible outcome value
+#Rename the two columns of nnet_blood.train with their respective binary meanings
 names(nnet_blood.train)[6] <- 'no'
 names(nnet_blood.train)[7] <- 'yes'
 
+#Check to make sure everything went out right
 head(nnet_blood.train)
 
-## Create neural network model (using one hidden layer with three nodes in this case)
+## Create neural network model
 nnet <- neuralnet(no + yes ~ recency.mo + frequency + blood.cc + time.mo,
                 data=nnet_blood.train, 
-                hidden=c(3))
+                hidden=c(1))
 
-## Take a gander at the network architecture and notice how it difference from the topology of the "concrete"
-## topology, which was a numerical prediction problem.
+#Generate the neural net output plot
 plot(nnet)
 
-## Compute predictions on entire iris dataset, including training (We can separate out the training data
-## out later, but let's just include it for our purposes)
+#Compute predictions on just the test set, rather than both training & test data
 pred <- compute(nnet, blood.test[,-5])$net.result
  
 # Put multiple binary output to categorical output
@@ -68,5 +71,14 @@ maxidx <- function(arr) {
 idx <- apply(pred, c(1), maxidx)
 prediction <- c(0,1)[idx]
 
-## View your results
+#View results in a confusion matrix
 table(prediction, blood.test$donated.2007)
+
+#Generate table with accuracy information
+nnodes <- c(0,1,2,3)
+accuracy <- c((136+5)/(136+5+8+28), NA, NA, NA)
+nnet.accuracy <- data.frame(nnodes, accuracy)
+
+#Plot Accuracy
+plot(nnet.accuracy,
+     xlab = "Number of Nodes", ylab = "Accuracy", main = "Number of Nodes vs. Accuracy")
