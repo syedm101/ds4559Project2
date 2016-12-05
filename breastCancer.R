a96eba1140d0f8871cd165eda49575169563b8a1
Mansoor Syed <syedm101@gmail.com>
2016-12-04 23:17:08 -0500

    Code for Question 2, C50 tree

--- breastCancer.R
+++ breastCancer.R
@@ -1,4 +1,4 @@
-#Read data from local folder
+#Read data from local folder, everything as a character to ease factoring
 bc<- read.csv("Wisconsin+BC+Data.txt", header=FALSE, colClasses = "character")
 View(bc)
 str(bc)
@@ -17,6 +17,7 @@ bc$V10 <- as.factor(bc$V10)
 bc$V11 <- as.factor(bc$V11)
 str(bc)
 
+#remove missing values
 bc <- subset(bc, V7!="?")
 #Set understandable column names
 colnames(bc) <- c("id","clumpThickness", "cellSizeUniformity","cellShapeUniformity","marginalAdhesion","epithelialCellSize","bareNuclei","blandChromatin","normalNucleoli","mitoses","class")
@@ -27,11 +28,12 @@ library(cvTools)
 k <- 10 #the number of folds
 datasetC5 <- bc
 set.seed(1234)
+#randomize data
 datasetC5_rand <- datasetC5[order(runif(683)),]
 
 ## Divide data into k folds:
 folds <- cvFolds(NROW(datasetC5_rand), K=k)
-
+#create predictions column to contain the probabilities for each instance
 datasetC5_rand$holdoutpred <- rep(0,nrow(datasetC5_rand))
 
 library(C50)
@@ -39,20 +41,29 @@ for(i in 1:k){
   train_dataC5 <- datasetC5_rand[folds$subsets[folds$which != i], ] #Set the training set
   validation_dataC5 <- datasetC5_rand[folds$subsets[folds$which == i], ] #Set the validation set
   
-  newmod <- C5.0(class ~ .,data=train_dataC5) #C5.0 model from non-kth fold
-  newpred <- predict(newmod,newdata=validation_dataC5[,-11]) #Get the predicitons for the validation set (from the model just fit on the train data)
+  newmod <- C5.0(class ~ .,data=train_dataC5,type="prob") #C5.0 model from non-kth fold
+  newpred <- predict(newmod,newdata=validation_dataC5[,-11],type="prob") #Get the probabilities for the validation set (from the model just fit on the train data)
   
-  datasetC5_rand[folds$subsets[folds$which == i], ]$holdoutpred <- newpred #Put the hold out prediction in the data set for later use.
+  datasetC5_rand[folds$subsets[folds$which == i], ]$holdoutpred <- newpred[,2] #Put the hold out probability in the data set for later use.
 }
 datasetC5_rand$holdoutpred
-
-newmod$output
-
 #2 is benign, 4 is malignant
-table(datasetC5_rand$holdoutpred,datasetC5_rand$class)
-# ~93 accuracy
 
+library(ROCR)
 
+#predictions for AUC calculation
+predictions <- datasetC5_rand$holdoutpred
+#true predictions for AUC calculation
+labels <- datasetC5_rand[,11]
+
+library(cvAUC)
+#Create cross validated area under ROC curve
+cvAUC(predictions,labels,label.ordering = NULL,folds)
+#area = .9411
+
+pred=prediction(predictions,labels)
+perf_ROC=performance(pred,"tpr","fpr") #plot the actual ROC curve
+plot(perf_ROC, main="ROC plot")
 
 
 
