361bd3104686da192799ebe18a70c1903555337c
Mansoor Syed <syedm101@gmail.com>
2016-12-05 00:36:50 -0500

    combined with wisconsin_andrew.r to form complete solution to question 2

--- breastCancer.R
+++ breastCancer.R
@@ -65,7 +65,40 @@ pred=prediction(predictions,labels)
 perf_ROC=performance(pred,"tpr","fpr") #plot the actual ROC curve
 plot(perf_ROC, main="ROC plot")
 
+#Random Forest Code
+library(randomForest)
+set.seed(1234)
 
+k <- 5 #the number of folds
+dataset <- bc
+dataset_rand <- dataset[order(runif(683)),]
+
+## Divide data into k folds:
+
+folds <- cvFolds(NROW(dataset), K=k)
+
+dataset_rand$holdoutpred <- rep(0,nrow(bc))
+
+for(i in 1:k){
+  train_data <- dataset_rand[folds$subsets[folds$which != i], ] #Set the training set
+  validation_data <- dataset_rand[folds$subsets[folds$which == i], ] #Set the validation set
+  
+  newmod <- randomForest(x=bc[,2:10], y=bc[,11], importance = TRUE, ntree = 1000, proximity=TRUE) #Get your new linear model (just fit on the train data)
+  newpred <- predict(newmod,newdata=validation_data[,-11]) #Get the predicitons for the validation set (from the model just fit on the train data)
+  
+  dataset_rand[folds$subsets[folds$which == i], ]$holdoutpred <- newpred #Put the hold out prediction in the data set for later use.
+}
+
+table(dataset_rand$holdoutpred, dataset_rand[,11])
+predictions=as.vector(newmod$votes[,2])
+pred=prediction(predictions,bc[,11])
+
+perf_AUC=performance(pred,"auc") #Calculate the AUC value
+AUC=perf_AUC@y.values[[1]]
+AUC
+perf_ROC=performance(pred,"tpr","fpr") #plot the actual ROC curve
+plot(perf_ROC, main="ROC plot")
+text(0.5,0.5,paste("AUC = ",format(AUC, digits=5, scientific=FALSE)))
 
 
 
